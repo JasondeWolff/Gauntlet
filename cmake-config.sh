7bcb092a27d1917080a622f5d168b8d176bb1ae9
ConorDamery <154240@buas.nl>
2024-06-24 20:01:36 +0000

    Fixed linux build

--- cmake-config.sh
+++ cmake-config.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-sudo apt-get update && sudo apt-get install -y cmake build-essential libxrandr-dev libxinerama-dev libxcursor-dev libxi-dev libgl1-mesa-dev libasound2-dev git
+sudo apt-get update && sudo apt-get install -y cmake build-essential libxrandr-dev libxinerama-dev libxcursor-dev libxi-dev libgl1-mesa-dev libasound2-dev
 cmake mkdir build
 cmake -B build -DCMAKE_CXX_COMPILER=g++ -DCMAKE_C_COMPILER=gcc -DCMAKE_BUILD_TYPE=Debug -S .
